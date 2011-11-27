f6998cd2882481e5db6272b4fdbba34589759bc3
Oleh Burkhay <oleg.burhay@gmail.com>
2011-11-27 02:15:57 -0300

    formatting fix

--- config/ipinfo.php
+++ config/ipinfo.php
@@ -5,12 +5,12 @@
  * @author Oleh Burkhay <atma@atmaworks.com>
  */
 return array(
-	/**
+    /**
      * ipinfodb key for remote API access
-	 * Can be registered at http://ipinfodb.com/account.php
-	 */
-	'key' => 'your ipinfodb.com API key',
-	'cache_time' => 3600*24*30,
+     * Can be registered at http://ipinfodb.com/account.php
+     */
+    'key' => 'your ipinfodb.com API key',
+    'cache_time' => 3600 * 24 * 30,
     'default_ip' => '80.247.32.206',
     'auto_localize' => true,
 );
